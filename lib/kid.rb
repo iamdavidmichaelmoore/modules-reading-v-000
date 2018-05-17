f6ed0c79ac38656bba697b7532a6ea90ef97695e
David Moore <hey@iamdavidmoore.com>
2018-05-17 20:44:00 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -4,6 +4,9 @@ require_relative './class_methods_module.rb'
 class Kid
   include Dance
 
+  # When we use the include keyword, we all our classes to use all of the methods of the included module as instance moethds.  
+  # However, to lend module's methods to a class as class methods, we use the extend keyword.
+
   attr_accessor :name
 
   def initialize(name)
