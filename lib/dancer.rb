a245a7d8bb6f72c9102237453c9d50972bd5c304
David Moore <hey@iamdavidmoore.com>
2018-05-17 21:03:04 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,9 +1,10 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
 
 class Dancer
-  include Dance
-  extend MetaDancing
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
   # When we use the include keyword, we all our classes to use all of the methods of the included module as instance moethds.
   # However, to lend module's methods to a class as class methods, we use the extend keyword.
 
